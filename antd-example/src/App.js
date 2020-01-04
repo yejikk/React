b57b09efeb6ca989466381114fcd14abd9f0613e
yejikk <dpwl7484@gmail.com>
2020-01-04 17:27:56 +0900

    정렬

--- antd-example/src/App.js
+++ antd-example/src/App.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Row, Col } from "antd";
 
-const colStyle = () => ({
-  height: 50,
-  backgroundColor: "red",
-  opacity: Math.round(Math.random() * 10) / 10
-});
+function MyCol({ span, offset }) {
+  const opacity = Math.round(Math.random() * 10) / 10;
+  return (
+    <Col span={span} offset={offset}>
+      <div style={{ height: 50, backgroundColor: "red", opacity }} />
+    </Col>
+  );
+}
 
 // 기본적으로 24
 function App() {
   return (
     <div className="App">
-      <Row>
-        <Col span={12} style={colStyle()} />
-        <Col span={12} style={colStyle()} />
-      </Row>
-      <Row>
-        <Col span={8} style={colStyle()} />
-        <Col span={8} style={colStyle()} />
-        <Col span={8} style={colStyle()} />
-      </Row>
-      <Row>
-        <Col span={6} style={colStyle()} />
-        <Col span={6} style={colStyle()} />
-        <Col span={6} style={colStyle()} />
-        <Col span={6} style={colStyle()} />
+      <Row
+        style={{
+          height: 300
+        }}
+        type="flex"
+        justify="center"
+        align="middle"
+      >
+        <MyCol span={4} />
+        <MyCol span={4} />
+        <MyCol span={4} />
+        <MyCol span={4} />
       </Row>
     </div>
   );
