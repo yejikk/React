cc6cecc8249f1ae2ad3a61264917fa94de8ae4b3
yejikk <dpwl7484@gmail.com>
2020-04-14 14:53:13 +0900

    props / state

--- react-code/src/App.js
+++ react-code/src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
 import "./App.css";
 
-class LifeCycle extends React.Component {
+// state - 해당하는 컴포넌트 안에서만 내부적으로 사용한다.
+// 다른 컴포넌트와의 데이터 교환을 하지 않고 외부와 관련없이 내부적으로 사용한다.
+
+// props - 상위 계층의 컴포넌트와 데이터 교환을 위해서 사용한다.
+class Fast extends React.Component {
   constructor(props) {
     super(props);
-    // 현재 클래스의 변화를 유도하는 비동기 함수
-    // render와 Unmount에서는 사용이 불가하다.
-    // 비동기적으로 실행되기 때문에 render에서 사용이 불가능하고
-    // Unmount는 사라지는 과정이기 때문에 사용이 불가능하다.
-    this.setState = {};
+    this.state = {
+      // state안에 props 데이터를 저장할 수 있다.
+      props,
+      lang: "javascript",
+      fn: function () {},
+      date: new Date(),
+    };
   }
 
-  // Render 함수가 이루어지기 직전 메소드
-  // 화면에 UI가 보여지기 전
-  componentWillMount() {}
-
-  // Rendering이 최초 1회 실행된 메소드
-  componentDidMount() {}
-
-  // 더이상 사용하지 않을 때 메모리 누수를 방지하기 위하여 작업을 초기화한다.
-  componentWillUnmount() {}
-
   render() {
-    return <> </>;
+    const { lang, date } = this.state;
+
+    return (
+      <div>
+        {lang} {date}
+      </div>
+    );
   }
 }
 
-export default LifeCycle;
+export default Fast;
