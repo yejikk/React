2ac42d0a5dfe1ae4433f1385de36e8c1b481db22
yejikk <dpwl7484@gmail.com>
2020-04-14 15:04:16 +0900

    event handling

--- react-code/src/App.js
+++ react-code/src/App.js
@@ -1,31 +1,26 @@
 import React from "react";
 import "./App.css";
 
-// state - 해당하는 컴포넌트 안에서만 내부적으로 사용한다.
-// 다른 컴포넌트와의 데이터 교환을 하지 않고 외부와 관련없이 내부적으로 사용한다.
+function App() {
+  const handleClick = (param) => {
+    // 이벤트 버블링을 방지하기 위하여
+    // 돔 객체에 대해서 이벤트가 실행이 될때 하나의 돔 객체가 내부 안에 있어서
+    // 외부와 중첩이 되는 구조로 되어있을 때, 해당 기능이 두번 이상 실행될 수 있기 때문에
+    // 이를 방지하기 위해 e.preventDefault()를 사용하여 정확한 이벤트 핸들링을 처리할 수 있다.
+    console.log(param);
+    console.log("button is clicked");
+  };
 
-// props - 상위 계층의 컴포넌트와 데이터 교환을 위해서 사용한다.
-class Fast extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // state안에 props 데이터를 저장할 수 있다.
-      props,
-      lang: "javascript",
-      fn: function () {},
-      date: new Date(),
-    };
-  }
+  const newhandleClick = (e) => {
+    e.preventDefault();
+    console.log("New button is clicked");
+  };
 
-  render() {
-    const { lang, date } = this.state;
-
-    return (
-      <div>
-        {lang} {date}
-      </div>
-    );
-  }
+  return (
+    <div onClick={newhandleClick}>
+      <button onClick={() => handleClick("param")}>this is a button</button>
+    </div>
+  );
 }
 
-export default Fast;
+export default App;
