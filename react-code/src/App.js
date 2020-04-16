af80da047b913349f1d4c2d213c705294b9d35b9
yejikk <dpwl7484@gmail.com>
2020-04-16 19:30:56 +0900

    setState

--- react-code/src/App.js
+++ react-code/src/App.js
@@ -1,26 +1,34 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  const handleClick = (param) => {
-    // 이벤트 버블링을 방지하기 위하여
-    // 돔 객체에 대해서 이벤트가 실행이 될때 하나의 돔 객체가 내부 안에 있어서
-    // 외부와 중첩이 되는 구조로 되어있을 때, 해당 기능이 두번 이상 실행될 수 있기 때문에
-    // 이를 방지하기 위해 e.preventDefault()를 사용하여 정확한 이벤트 핸들링을 처리할 수 있다.
-    console.log(param);
-    console.log("button is clicked");
-  };
+// setState는 Render 함수 안에서 사용할 수 없다.
+// mount, update에서 setState를 호출 할 수 있다.
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(),
+    };
+  }
 
-  const newhandleClick = (e) => {
-    e.preventDefault();
-    console.log("New button is clicked");
-  };
+  // ui 초기화
+  componentDidMount() {}
 
-  return (
-    <div onClick={newhandleClick}>
-      <button onClick={() => handleClick("param")}>this is a button</button>
-    </div>
-  );
+  tick() {
+    this.setState(
+      {
+        time: new Date(),
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  }
+
+  render() {
+    const { time } = this.state;
+    return <div>{time.date.toLocaleTimeString()}</div>;
+  }
 }
 
-export default App;
+export default Timer;
